c9d7892fe7961242ab4a80f639afe1acf659d47f
Jake Cronin <jake.cronin4@gmail.com>
2023-09-04 14:00:34 -0400

    fix lint error

--- functions/tests/persistence.test.ts
+++ functions/tests/persistence.test.ts
@@ -1,13 +1,7 @@
 
-import {getRecording, getUser} from "../src/persistence";
+import {getUser} from "../src/persistence";
 
 describe("handleTrackRecordingView Tests", () => {
-  it("Loads a recording document", async () => {
-    const recording = await getRecording("recordingA");
-    console.log("Got recording: ", recording);
-    return expect(recording).toBeDefined();
-  });
-
   it("Loads a user document", async () => {
     const user = await getUser("Joe");
     console.log("Got user: ", user);
